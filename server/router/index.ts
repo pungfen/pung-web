54bff5f3dd647302bb23de7521ac1c551d6c16b8
pung <fp94214@live.com>
2021-01-19 02:33:55 +0800

    ~

--- server/router/index.ts
+++ server/router/index.ts
@@ -1,24 +1,37 @@
 import * as Router from 'koa-router'
 
-import * as Nedb from 'nedb-promise'
+import * as Nedb from 'nedb'
+
+// import { db, datastore } from '../lib/nedb'
 
 const DB = {
-  user: new Nedb({ filename: 'data/user.db', autoload: true, timestampData: true })
+  user: new Nedb({
+    filename: 'data/user.db',
+    autoload: true,
+    timestampData: true,
+  }),
+  // note: Nedb.datastore({
+  //   filename: 'data/note.db',
+  //   autoload: true,
+  //   timestampData: true,
+  // }),
 }
 
 const router = new Router()
 
-router
-  .get('/user', async (ctx) => {
-    ctx.body = await DB.user.find({ })
-  })
-  .post('/user', async (ctx) => {
-    const { uuid } = ctx.request.body
-    if (uuid) {
-      const current = await DB.user.find({ uuid })
-      if (current) ctx.body = current
-      else ctx.body = await DB.user.insert({ uuid })
-    }
-  })
+router.get('/user', async (ctx) => {
+  ctx.body = await thenify(DB.user.find({}, (err, res) => res))
+})
+// .get('/note', async (ctx) => {
+//   ctx.body = await DB.note.find({})
+// })
+// .post('/user', async (ctx) => {
+//   const { uuid } = ctx.request.body
+//   if (uuid) {
+//     const current = await DB.user.find({ uuid })
+//     if (current) ctx.body = current
+//     else ctx.body = await DB.user.insert({ uuid })
+//   }
+// })
 
 export const routes = router.routes()
