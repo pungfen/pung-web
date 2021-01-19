edf1a13f9ce37ad6231ea88154210ebe57c42d55
pungfen <fp94214@live.com>
2021-01-19 19:33:35 +0800

    ~

--- server/router/index.ts
+++ server/router/index.ts
@@ -8,11 +8,19 @@ const DB = {
     autoload: true,
     timestampData: true,
   }),
+
+  note: nedb({
+    filename: 'data/note.db',
+    autoload: true,
+    timestampData: true,
+  }),
 }
 
 const router = new Router()
 
 router
+  .prefix('/api')
+
   .get('/user', async (ctx) => {
     ctx.body = await DB.user.find({})
   })
@@ -21,7 +29,26 @@ router
     if (uuid) {
       const current = await DB.user.find({ uuid })
       if (current) ctx.body = current
-      else ctx.body = await DB.user.insert({ uuid })
+      else ctx.body = await DB.user.update({ uuid })
+    }
+  })
+
+  .get('/note', async (ctx) => {
+    ctx.body = await DB.note.find({})
+  })
+  .get('/note/:uuid', async (ctx) => {
+    const { uuid } = ctx.request.body
+    ctx.body = await DB.note.find({ uuid })
+  })
+  .post('/note', async (ctx) => {
+    const { uuid, content } = ctx.request.body
+    // if (uuid) {
+    //   const user = await
+    // }
+    if (content) {
+      ctx.body = await DB.note.update({ uuid }, { $set: { content } })
+    } else {
+      ctx.body = []
     }
   })
 
