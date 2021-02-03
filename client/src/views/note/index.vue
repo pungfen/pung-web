7bfc91d50dbd08e23ad18d4e243b6d2c6194d10f
pung <fp94214@live.com>
2021-02-04 01:22:08 +0800

    ~

--- client/src/views/note/index.vue
+++ client/src/views/note/index.vue
@@ -3,28 +3,39 @@
     <h1>笔记</h1>
   </div>
 
-  <el-table :data="table.data" :show-header="false">
-    <el-table-column>
-      <template #default="{ row }">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="content"></el-table-column>
-    <el-table-column>
-      <template #default="{ row }">
-        <el-button type="text">查看</el-button>
-        <el-button type="text">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <flex-box v-model:height="table.height">
+    <el-table :data="table.data" :show-header="false">
+      <el-table-column>
+        <template #default="{ row }">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="content"></el-table-column>
+      <el-table-column>
+        <template #default="{ row }">
+          <el-button type="text">查看</el-button>
+          <el-button type="text">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </flex-box>
 </template>
 
 <script>
+import FlexBox from '@/options/components/project/flex-box'
+
 export default {
+  components: {
+    FlexBox
+  },
   data() {
     return {
+      flexBox: {
+        height: 0
+      },
       table: {
+        height: 10,
         data: Array(30).fill({ date: '2021-1-22', content: '哈哈江哈斯的卡上打开链接啥的' })
       }
     }
