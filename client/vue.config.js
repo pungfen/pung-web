73cc60b4ebac48f41cdea8a604ed9b03b152fcfb
pungfen <fp94214@live.com>
2021-02-09 12:53:08 +0800

    ~

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = () => {
   const config = {
     lintOnSave: process.env.NODE_ENV !== 'production',
+    productionSourceMap: false,
     devServer: {
       port: '8088',
       proxy: 'http://localhost:3000'
