934bc3ffb51fe8363720216eb75b1dfefe0e8228
pungfen <fp94214@live.com>
2021-02-22 11:44:16 +0800

    ~

---
+++ blog/docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'PUNG - 请让我知道，何事美丽至此',
+
+  description: 'It is pung blog',
+
+  themeConfig: {
+    logo: '/logo.png',
+    docsDir: 'docs',
+    lastUpdated: '上次编辑',
+    sidebar: {}
+  }
+}
