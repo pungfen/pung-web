a5bbe1048d74079296497c9a0711c3cda0a7ec8a
pungfen <fp94214@live.com>
2021-02-24 19:03:30 +0800

    ~

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,8 +1,8 @@
 export const config = {
   port: 3000,
   db: {
-    root: 'pung',
-    password: 'mysql@94214',
+    root: 'root',
+    password: 'root',
     host: '47.114.7.50',
     scheme: 'pung_web'
   }
